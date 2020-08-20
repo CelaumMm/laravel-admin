af9422c211586696be62255293878fccb814ff75
Marcelo Camargo <m.camargo@reply.com>
2020-08-20 19:25:01 -0300

    Adicionado o email verificado na criação dos usuários

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -16,12 +17,16 @@ class UserSeeder extends Seeder
             'name'      => 'Administrador master',
             'email'     => 'admin@admin.com',
             'password'  => bcrypt('12345678'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ]);
 
         User::create([
             'name'      => 'Usuario comum',
             'email'     => 'user@user.com',
             'password'  => bcrypt('12345678'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ]);
     }
 }
