73aaf7ff15d68c07ed4e0e6f7f0304a901e8004e
Marcelo Camargo <m.camargo@reply.com>
2020-08-20 21:54:18 -0300

    Configurações adicionais do pacote larevel telescope

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
 
         // PACKAGE LARAVEL PERMISSION
         Spatie\Permission\PermissionServiceProvider::class,
