1d8c8c1ffbf476f6ff2a710a4b784e4dd2968ff4
Marcelo Camargo <m.camargo@reply.com>
2020-08-20 21:36:01 -0300

    Adicionando os assets e config do pacote Laravel telescope

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
         // PACKAGE LARAVEL PERMISSION
         Spatie\Permission\PermissionServiceProvider::class,
