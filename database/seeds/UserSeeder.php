e07e0098e29575cf0a23d90e64c11d15b1509662
Marcelo Camargo <m.camargo@reply.com>
2020-08-20 22:20:22 -0300

    Adicionando o usuario Administrador a função de admin

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,20 +13,20 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name'      => 'Administrador master',
-            'email'     => 'admin@admin.com',
-            'password'  => bcrypt('12345678'),
+		User::create([
+            'name'      		=> 'Administrador master',
+            'email'     		=> 'admin@admin.com',
+            'password'  		=> bcrypt('12345678'),
             'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
-        ]);
+            'remember_token' 	=> Str::random(10),
+        ])->assignRole(1);
 
         User::create([
-            'name'      => 'Usuario comum',
-            'email'     => 'user@user.com',
-            'password'  => bcrypt('12345678'),
+            'name'      		=> 'Usuario comum',
+            'email'     		=> 'user@user.com',
+            'password'  		=> bcrypt('12345678'),
             'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
+            'remember_token' 	=> Str::random(10),
         ]);
     }
 }
