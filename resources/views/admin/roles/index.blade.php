28675f109e62567fee9a854ed1152aa09fd29bd1
Marcelo Camargo <m.camargo@reply.com>
2020-09-02 03:37:33 -0300

    Adicionando as view roles

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -9,13 +9,13 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1><i class="fas fa-user-tag"></i> @lang('admin.roles.list')</h1>
+                <h1><i class="fas fa-list"></i> @lang('admin.roles.list')</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{ route('admin.home') }}"><i class="fas fa-home"></i> Home</a>
                     </li>
-                    <li class="breadcrumb-item active"><i class="fas fa-user-tag"></i> @lang('admin.roles.list')</li>
+                    <li class="breadcrumb-item active"><i class="fas fa-list"></i> @lang('admin.roles.list')</li>
                 </ol>
             </div>
         </div>
